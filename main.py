e24325f37cb9860b4f7d50343a787e5b4e9c6262
sexbomb <arsbar91@gmail.com>
2024-06-16 16:41:19 +0700

    kl

--- main.py
+++ main.py
@@ -1,68 +1,69 @@
 from kivy.app import App
 from kivy.uix.widget import Widget
 from kivy.graphics import Line, Color
+from kivy.uix.button import Button
 from kivy.uix.boxlayout import BoxLayout
-from kivy.uix.slider import Slider
 from kivy.uix.colorpicker import ColorPicker
+from kivy.uix.slider import Slider
 
 
 class PaintWidget(Widget):
     def __init__(self, **kwargs):
-        super().__init__(**kwargs)
-        self.line_width = 2
-        self.color = (1, 0, 0, 1)  # Default to red color
+        super(PaintWidget, self).__init__(**kwargs)
+        self.line_width = 1
+        self.color = (0, 0, 0, 1)
+        self.canvas.before.add(Color(*self.color))
 
     def on_touch_down(self, touch):
-        if self.collide_point(*touch.pos):
-            with self.canvas:
-                Color(*self.color)
-                touch.ud['line'] = Line(points=(touch.x, touch.y), width=self.line_width)
-            return True
-        return super().on_touch_down(touch)
+        with self.canvas:
+            Color(*self.color)
+            touch.ud['line'] = Line(points=(touch.x, touch.y), width=self.line_width)
 
     def on_touch_move(self, touch):
-        if 'line' in touch.ud:
-            touch.ud['line'].points += [touch.x, touch.y]
-            return True
-        return super().on_touch_move(touch)
+        touch.ud['line'].points += [touch.x, touch.y]
 
-    def set_color(self, color):
-        self.color = color
-
-    def set_line_width(self, width):
-        self.line_width = width
+    def clear_canvas(self):
+        self.canvas.clear()
+        self.canvas.before.add(Color(*self.color))
 
 
 class PaintApp(App):
     def build(self):
         root = BoxLayout(orientation='vertical')
-        self.paint_widget = PaintWidget()
-        root.add_widget(self.paint_widget)
+        self.painter = PaintWidget()
 
-        # Create controls
-        controls = BoxLayout(size_hint=(1, 0.2))
+        # Creating the top layout for controls
+        controls_layout = BoxLayout(size_hint_y=0.2)
 
-        # Add color picker
-        self.color_picker = ColorPicker(size_hint=(0.7, 1))
+        # Add ColorPicker
+        self.color_picker = ColorPicker(size_hint=(0.4, 1))
         self.color_picker.bind(color=self.on_color)
+        controls_layout.add_widget(self.color_picker)
 
-        # Add line width slider
-        self.line_width_slider = Slider(min=1, max=10, value=2, size_hint=(0.3, 1))
-        self.line_width_slider.bind(value=self.on_line_width)
+        # Add Line Width Slider
+        self.slider = Slider(min=1, max=10, value=1, size_hint=(0.4, 1))
+        self.slider.bind(value=self.on_line_width)
+        controls_layout.add_widget(self.slider)
 
-        # Add buttons and controls to layout
-        controls.add_widget(self.color_picker)
-        controls.add_widget(self.line_width_slider)
+        # Add Clear Button
+        clear_btn = Button(text='Clear', size_hint=(0.2, 1))
+        clear_btn.bind(on_release=self.clear_canvas)
+        controls_layout.add_widget(clear_btn)
 
-        root.add_widget(controls)
+        # Add the controls and the painter to the root layout
+        root.add_widget(controls_layout)
+        root.add_widget(self.painter)
 
         return root
 
     def on_color(self, instance, value):
-        self.paint_widget.set_color(value)
+        self.painter.color = value
 
     def on_line_width(self, instance, value):
-        self.paint_widget.set_line_width(value)
+        self.painter.line_width = value
+
+    def clear_canvas(self, instance):
+        self.painter.clear_canvas()
 
 
 if __name__ == '__main__':
